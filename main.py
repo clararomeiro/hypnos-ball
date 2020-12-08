385bcddb5ea177bde3c9c076caf0f612a0282a9b
Ana Clara Romeiro <clararomeirocosta@gmail.com>
2020-12-08 17:06:32 -0300

    Ininializando posições

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ pygame.init()
 window = pygame.display.set_mode((pygame.RESIZABLE, pygame.RESIZABLE), pygame.FULLSCREEN)
 pygame.display.set_caption("Hypnos Ball")
 
-# Definindo função draw
-
-
-def draw():
-    window.fill([255, 255, 255])
-
 
 window_open = True
+pos_x = 60
+pos_y = 60
+vel = 1
 
 while window_open:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 window_open = False
+            elif event.key == pygame.K_DOWN and pos_y == 60:
+                pos_y = 500
+            elif event.key == pygame.K_DOWN and pos_y == 500:
+                pos_y = 1010
+            elif event.key == pygame.K_UP and pos_y == 1010:
+                pos_y = 500
+            elif event.key == pygame.K_UP and pos_y == 500:
+                pos_y = 60
 
-    draw()
+    # Desenhando
+    window.fill([255, 255, 255])
+    pos_x = pos_x + vel
+    pygame.draw.circle(window, (0, 100, 158), (pos_x, pos_y), 50)
     pygame.display.update()
