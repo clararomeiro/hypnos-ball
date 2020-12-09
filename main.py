6f9c07cfeecad4ac345d129722ff10bc674c9c1b
Ana Clara Romeiro <clararomeirocosta@gmail.com>
2020-12-08 21:53:06 -0300

    Update dimensões

--- main.py
+++ main.py
@@ -10,33 +10,41 @@ pygame.display.set_caption("Hypnos Ball")
 window_open = True
 pos_x = 85
 pos_y = 85
-vel = 10
+vel = 0.3
 clock = pygame.time.Clock()
 
-while window_open:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            window_open = False
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            if pos_y == 85:
-                pos_y = 500
-            elif pos_y == 500:
-                pos_y = 990
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            if pos_y == 990:
-                pos_y = 500
-            elif pos_y == 500:
-                pos_y = 85
+if __name__ == "__main__":
+    while window_open:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                window_open = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                if pos_y == 85:
+                    pos_y = (window.get_height()-85)/2
+                elif pos_y == (window.get_height()-85)/2:
+                    pos_y = window.get_height()-85
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                if pos_y == window.get_height()-85:
+                    pos_y = (window.get_height()-85)/2
+                elif pos_y == (window.get_height()-85)/2:
+                    pos_y = 85
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1 or event.key == pygame.K_KP1:
+                    vel = 0.3
+                elif event.key == pygame.K_2 or event.key == pygame.K_KP2:
+                    vel = 0.5
+                elif event.key == pygame.K_3 or event.key == pygame.K_KP3:
+                    vel = 0.7
 
-    if pos_x == 1800:
-        sleep(0.5)
-        pos_x = 85
-    elif pos_x == 85:
-        sleep(0.5)
-        pos_x = 1800
+        if pos_x == window.get_width()-85:
+            sleep(vel)
+            pos_x = 85
+        elif pos_x == 85:
+            sleep(vel)
+            pos_x = window.get_width()-85
 
-    # Desenhando
-    window.fill([255, 255, 255])
-    pygame.draw.circle(window, (0, 100, 158), (pos_x, pos_y), 75)
-    pygame.display.update()
+        # Desenhando
+        window.fill([255, 255, 255])
+        pygame.draw.circle(window, (201, 26, 26), (pos_x, pos_y), 75)
+        pygame.display.update()
