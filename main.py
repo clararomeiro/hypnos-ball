ea0f0a5272a549818ead15b45a69774a51833f99
Ana Clara Romeiro <clararomeirocosta@gmail.com>
2020-12-08 21:14:39 -0300

    Update movimento

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from time import sleep
 
 # Iniciando Janela
 pygame.init()
@@ -7,26 +8,35 @@ pygame.display.set_caption("Hypnos Ball")
 
 
 window_open = True
-pos_x = 60
-pos_y = 60
-vel = 1
+pos_x = 85
+pos_y = 85
+vel = 10
+clock = pygame.time.Clock()
 
 while window_open:
+    clock.tick(60)
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                window_open = False
-            elif event.key == pygame.K_DOWN and pos_y == 60:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            window_open = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            if pos_y == 85:
                 pos_y = 500
-            elif event.key == pygame.K_DOWN and pos_y == 500:
-                pos_y = 1010
-            elif event.key == pygame.K_UP and pos_y == 1010:
+            elif pos_y == 500:
+                pos_y = 990
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            if pos_y == 990:
                 pos_y = 500
-            elif event.key == pygame.K_UP and pos_y == 500:
-                pos_y = 60
+            elif pos_y == 500:
+                pos_y = 85
+
+    if pos_x == 1800:
+        sleep(0.5)
+        pos_x = 85
+    elif pos_x == 85:
+        sleep(0.5)
+        pos_x = 1800
 
     # Desenhando
     window.fill([255, 255, 255])
-    pos_x = pos_x + vel
-    pygame.draw.circle(window, (0, 100, 158), (pos_x, pos_y), 50)
+    pygame.draw.circle(window, (0, 100, 158), (pos_x, pos_y), 75)
     pygame.display.update()
